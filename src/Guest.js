92d2904abfd0fc3d850c4779ad6d5a565d22a627
Aravind071845 <727721euec011@skcet.ac.in>
2023-04-04 23:47:43 +0530

    msg

--- src/Guest.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Guest() {
-  return (
-    <div>
-        <h1>Hello user,Login for better use</h1>
-    </div>
-  )
-}
--- src/LoginUser.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function LoginUser() {
-  return (
-    <div>
-        <h2>Hello user,You are logged in</h2>
-    </div>
-  )
-}
